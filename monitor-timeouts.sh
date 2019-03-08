06485a036bf2306c84596c249198a23221af48e9
superman-lopez <43129263+superman-lopez@users.noreply.github.com>
2019-03-08 14:59:42 +0800

    Update monitor-timeouts.sh

--- monitor-timeouts.sh
+++ monitor-timeouts.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 start=`date +%s`
 minutes=""
 timeouts=0
@@ -13,9 +12,9 @@ echo "Start monitor for network timeouts at `date` for $minutes minute(s)"
 
 while [ $(( $(date +%s) - (60 * minutes) )) -lt $start ]
 do
-	ping=`ping google.com -c 1 | head -2 | tail -1`
+	ping=`ping google.com -c 1 | head -5`
 
-	if [[ $ping == *"timeout"* ]]; then
+	if [[ $ping == *"0 packets received"* ]]; then
 		date
 		echo $ping
 		((timeouts++))
